89c070e1436ab6e7b581bba0acc2dd4ecda557f5
revaldiii <revaldirifwianda@gmail.com>
2024-09-04 15:56:39 +0700

    Update 4-nested_if_statement.py

--- Week4/4-nested_if_statement.py
+++ Week4/4-nested_if_statement.py
@@ -5,7 +5,7 @@ if jenis_kelamin.lower() in ['pria', "laki laki", "cowok"]:
     if (umur >= 25):
         print (str(jenis_kelamin), " boleh menikah")
     else:
-        print (str(jenis_kelamin), " boleh menikah")
+        print (str(jenis_kelamin), " tidak boleh menikah")
 elif jenis_kelamin.lower() in ['wanita', "perempuan", "cewek"] :
     umur = int(input("Umur = "))
     if (umur >= 20):
