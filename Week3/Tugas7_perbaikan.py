7a0863fa06e935761c3f902400d1422f3f9a2fdb
revaldiii <revaldirifwianda@gmail.com>
2024-09-11 10:54:39 +0700

    Create Tugas7_perbaikan.py

---
+++ Week3/Tugas7_perbaikan.py
@@ -0,0 +1,27 @@
+z = [1, 3, 2, 4, ]
+z.sort()
+print(z)
+
+print()
+
+print("Hello there!\nHow are you?\nI\'m doing fine.")
+
+multi_line = """Hello there!
+How are you?
+I'm fine."""
+print()
+print(multi_line)
+
+print()
+spam = ' Hello World '
+print(spam.strip())
+print(spam.lstrip())
+print(spam.rstrip())
+
+print()
+print(', '.join(['cats', 'rats', 'bats']))
+print(' '.join(['My', 'name', 'is', 'Simon']))
+print('ABC'.join(['My', 'name', 'is', 'Simon']))
+print('My name is Simon'.split())
+print('MyABCnameABCisABCSimon'.split('ABC'))
+print('My name is Simon'.split('m'))
