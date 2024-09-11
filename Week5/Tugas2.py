8fc0d598685db31c43dd3c263f13197c99677428
revaldiii <revaldirifwianda@gmail.com>
2024-09-11 19:35:03 +0700

    Update Tugas2.py

--- Week5/Tugas2.py
+++ Week5/Tugas2.py
@@ -8,17 +8,26 @@ while True:
         print("Sisa saldo =", sisa)  # Sisa bila di berhentikan
         break
 
+    # Cek jika pengeluaran lebih besar dari sisa saldo
+    while pengeluaran > sisa:
+        print("Saldo tidak cukup")
+        print("Sisa saldo", sisa)
+        pengeluaran = int(input("Masukan pengeluaran yang valid (-1 untuk keluar): "))
+        if pengeluaran == -1:  # Untuk berhenti jika pengguna memutuskan
+            print("Sisa saldo =", sisa)  # Sisa bila di berhentikan
+            break
+        if pengeluaran <= sisa:
+            break  # Keluar dari loop saat pengeluaran valid
+
+    if pengeluaran == -1:  # Cek lagi setelah keluar dari loop jika saldo cukup
+        break
+
     sisa -= pengeluaran
 
-    if sisa < 0:
-        print("Saldo tidak cukup")
-        print("Sisa saldo", sisa + pengeluaran)  # Mengembalikan saldo ke nilai sebelum pengeluaran
-        break
-    
     # Tampilkan sisa saldo setelah pengeluaran
     print("Sisa saldo setelah pengeluaran:", sisa)
     
     # Jika saldo habis, program berhenti
     if sisa == 0:
-        print("Saldo habis.)
+        print("Saldo habis.")
         break
